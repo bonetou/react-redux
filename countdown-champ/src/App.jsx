5395fead2dd4c7370cd2644da0dce8b5ecee2bb8
bonetou <h.boneto@outlook.com>
2022-02-16 00:56:53 -0300

    feat: add boostrap to project

--- countdown-champ/src/App.jsx
+++ countdown-champ/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Clock from "./Clock";
-
+import { Form, FormControl, Button } from "react-bootstrap"
 
 class App extends Component {
     constructor(props) {
@@ -25,13 +25,16 @@ class App extends Component {
                 <Clock 
                     deadline={this.state.deadline}
                 />
-                <input 
-                    placeholder="new date"
-                    onChange={event => this.setState({newDeadline: event.target.value})}
-                />
-                <button onClick={() => this.changeDeadline()}>
-                    Submit
-                </button>
+                <Form>
+                    <FormControl
+                        className="Deadline-input"
+                        placeholder="new date"
+                        onChange={event => this.setState({newDeadline: event.target.value})}
+                    />
+                    <Button onClick={() => this.changeDeadline()}>
+                        Submit
+                    </Button>
+                </Form>
             </div>
         )
     }
